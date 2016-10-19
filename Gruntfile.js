e637fdbb000c114edb9929df2c4f7b925390f207
GaylouM <gaylord.marville@gmail.com>
2016-10-19 17:34:15 +0200

    Init

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'gm',
+          sizes: [{
+            rename: false,
+            width: 640,
+            suffix: '_640',
+            quality: 60
+          },{
+            rename: false,
+            width: 1024,
+            suffix: '_1024',
+            quality: 80
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
